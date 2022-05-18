f17fe6982e7d7845047c0dafc4f9fdfa743559c9
Zhenya <evgeegai@gmail.com>
2022-05-18 20:48:41 +0300

    feat: implement task 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,8 @@
-.
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const pathToFile = path.join(__dirname, 'text.txt');
+
+const newReadStream = fs.createReadStream(pathToFile);
+newReadStream.on('data', chunk => stdout.write(chunk));
