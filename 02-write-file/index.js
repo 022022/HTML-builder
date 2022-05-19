40ddb6f9a9e5f0aa9b7278c01e837a0257582d6a
Zhenya <evgeegai@gmail.com>
2022-05-19 18:44:18 +0300

    feat: implement task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const { createWriteStream } = require('fs');
+const { join } = require('path');
+const { stdout, stdin } = process;
+
+const output = createWriteStream(join(__dirname, 'txt-from-console.txt'));
+
+stdout.write('Please type some text here: ');
+
+stdin.on('data', data => {
+  if(data.toString().trim() === 'exit'){
+    process.exit();
+  }
+
+  output.write(data);
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+process.on('exit', () => console.log('Goodbye!'));
