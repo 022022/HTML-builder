02c661100da6127c5633148e14e2026e5b155e5b
Zhenya <evgeegai@gmail.com>
2022-05-20 19:55:12 +0300

    feat: implement task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+async function copyDir(){
+  const { readdir, mkdir, copyFile , rm } = require('fs/promises');
+  const { join } = require('path');
 
+  const oldDirPath = join(__dirname, 'files');
+  const newDirPath = join(__dirname, 'files-copy');
+
+  await rm(newDirPath, { recursive: true, force: true });
+
+  mkdir(newDirPath, {recursive: true});
+
+  try{
+    const files = await readdir(oldDirPath);
+
+    for (const file of files){
+      const oldFile = join(oldDirPath, file);
+      const newFile = join(newDirPath, file);
+      await copyFile(oldFile, newFile);
+    }
+
+  } catch (err){
+    console.log(err);
+  }
+
+}
+
+copyDir();
